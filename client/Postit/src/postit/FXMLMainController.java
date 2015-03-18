7e3383bdde8073e34d8411c27aa3956f5c5750f2
Arnaud <abracchetti@gmail.com>
2015-03-18 16:52:53 +0100

    Init du projet

---
+++ client/Postit/src/postit/FXMLMainController.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package postit;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+/**
+ *
+ * @author Arnaud
+ */
+public class FXMLMainController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+     @FXML
+    private VBox Col1;
+
+    @FXML
+    private VBox col2;
+    
+
+    private VBox col[];
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        try{
+        
+            Pane postit = FXMLLoader.load(getClass().getResource("FXMLPostit.fxml"));
+            Col1.getChildren().add(postit);
+            postit = FXMLLoader.load(getClass().getResource("FXMLPostit.fxml"));
+            Col1.getChildren().add(postit);
+        }    
+        catch(IOException e)
+        {
+        
+        }
+    }
+    
+}
---
+++ client/Postit/src/postit/Postit.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package postit;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import jssc.SerialPort;
+import jssc.SerialPortException;
+import jssc.SerialPortList;
+import jssc.SerialPortEvent;
+
+/**
+ *
+ * @author Arnaud
+ */
+public class Postit extends Application {
+    
+    private SerialPort sPort;
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        serial();  
+        
+        
+        Pane root = FXMLLoader.load(getClass().getResource("FXMLMain.fxml"));
+        
+        
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       
+      
+        
+    launch(args);
+    }
+    
+    private void serial()
+    {
+        String[] ports = SerialPortList.getPortNames();
+        sPort = new SerialPort("COM4");
+        
+        try {
+        sPort.openPort();    
+        sPort.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+        sPort.addEventListener(e-> {this.TheEventHandler(e);});
+        
+        }
+        catch(SerialPortException e)
+        {
+            System.out.println(e.getMethodName()+" : "+ e.getExceptionType());
+        } 
+    }
+    
+    private void TheEventHandler(SerialPortEvent e)
+    {
+        try 
+        {
+            if (e.isRXCHAR())
+            {
+                System.out.println(sPort.readString());
+            }
+        
+        }
+        catch(SerialPortException exp)
+        {
+            System.out.println(exp.getMethodName()+" : "+ exp.getExceptionType());
+        } 
+    }
+}

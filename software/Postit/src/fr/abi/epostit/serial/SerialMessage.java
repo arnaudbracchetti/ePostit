0cf6a2ecef85956aaf15d21fda01b19b7650989d
Arnaud BRACCHETTI <abracchetti@gmail.com>
2015-03-29 20:03:46 +0200

    Suppression d'import inutils

--- software/Postit/src/fr/abi/epostit/serial/SerialMessage.java
+++ software/Postit/src/fr/abi/epostit/serial/SerialMessage.java
@@ -5,11 +5,6 @@
  */
 package fr.abi.epostit.serial;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
